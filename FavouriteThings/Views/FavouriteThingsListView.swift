46890df4ae91b15b1d4c6653a6835ff78a607d11
Isaad Khan <IKhan@lcs.on.ca>
2022-11-18 09:06:53 -0500

    Navigation view

--- FavouriteThings/Views/FavouriteThingsListView.swift
+++ FavouriteThings/Views/FavouriteThingsListView.swift
@@ -10,66 +10,143 @@ import SwiftUI
 
 struct FavouriteThingsListView: View {
     var body: some View {
-        
-        NavigationView {
-            List {
-                
-    //            Text("Basketball")
-    //            Text("Soccer")
-    //            Text("Islam")
-                
-                NavigationLink(destination: {
-                    Basketball()
-                }, label: {
-                    HStack {
-                    Image("Basketball")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 75, height: 50, alignment: .center)
-                        VStack(alignment: .leading) {
+        VStack {
+            
+          
+            NavigationView {
+                List {
+                    Text("My Favorite things")
+                        .font(.largeTitle)
+                    NavigationLink(destination: {
+                        Basketball()
+                    }, label: {
+                        HStack {
+                            Image("Basketball")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
                                 Text("Basketball")
                                     .bold()
-                                    
+                                
                                 
                                 Text("Basketball is my favorite sport to play.")
                                 
-                            
+                                
+                                
+                            }
                             
                         }
-
-                    }
-                })
-                
-                NavigationLink(destination: {
-                    Soccer()
-                }, label: {
-                    Text("Soccer")
-                })
-                
-                NavigationLink(destination: {
-                    Islam()
-                }, label: {
-                    Text("Islam")
-                })
-                
-                NavigationLink(destination: {
-                    KFC()
-                }, label: {
-                    Text("KFC")
-                })
-                
-                NavigationLink(destination: {
-                    Youtube()
-                }, label: {
-                    Text("Youtube")
-                })
-                
-                NavigationLink(destination: {
-                    Reese_Sticks()
-                }, label: {
-                    Text("Reese Sticks")
-                })
-                
+                    })
+                    
+                    NavigationLink(destination: {
+                        Soccer()
+                    }, label: {
+                        HStack {
+                            Image("Soccer")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
+                                Text("Soccer")
+                                    .bold()
+                                
+                                
+                                Text("Soccer is my favorite sport to watch.")
+                            }
+                            
+                        }
+                    })
+                    
+                    NavigationLink(destination: {
+                        Islam()
+                    }, label: {
+                        HStack {
+                            Image("Islam")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
+                                Text("Iskam")
+                                    .bold()
+                                
+                                
+                                Text("I live by the rules of Islam and worship god.")
+                                
+                                
+                                
+                            }
+                            
+                        }
+                    })
+                    
+                    NavigationLink(destination: {
+                        KFC()
+                    }, label: {
+                        HStack {
+                            Image("KFC")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
+                                Text("KFC")
+                                    .bold()
+                                
+                                
+                                Text("KFC is my favorite fast food chain.")
+                                
+                                
+                                
+                            }
+                            
+                        }
+                    })
+                    
+                    NavigationLink(destination: {
+                        Youtube()
+                    }, label: {
+                        HStack {
+                            Image("Youtube")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
+                                Text("Youtube")
+                                    .bold()
+                                
+                                
+                                Text("Youtube quenches my entertainment needs.")
+                                
+                                
+                                
+                            }
+                            
+                        }
+                    })
+                    
+                    NavigationLink(destination: {
+                        Reese_Sticks()
+                    }, label: {
+                        HStack {
+                            Image("Reese Sticks")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 75, height: 50, alignment: .center)
+                            VStack(alignment: .leading) {
+                                Text("Reese Sticks")
+                                    .bold()
+                                
+                                
+                                Text("Reese sticks is my favorite chocolate.")
+                                
+                                
+                                
+                            }
+                            
+                        }
+                    })
+                    
+                }
             }
         }
         .navigationTitle("My Favorite Things")
